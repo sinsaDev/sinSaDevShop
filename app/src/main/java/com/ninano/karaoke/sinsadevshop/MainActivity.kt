4eb3a2209bb314c8c1b871c12eb7f19ea6515993
ent-min <msj349911@gmail.com>
2020-07-06 18:21:18 +0900

    sinsaDevShop Android First Commit

---
+++ app/src/main/java/com/ninano/karaoke/sinsadevshop/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.ninano.karaoke.sinsadevshop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    var name: String ="안녕하세요"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
